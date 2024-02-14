d84468eee9ccf24ca8b4fbccb3665550c39f6952
rlobz <rlobzowski@gmail.com>
2024-02-14 14:51:43 +0000

    created Thought to model

---
+++ models/Thought.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new mongoose.Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => createdAtVal.toISOString(),
+    },
+}, {
+    toJSON: {
+        getters: true,
+    },
+    id: false,
+});
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => createdAtVal.toISOString(),
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [reactionSchema],
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true,
+    },
+    id: false,
+});
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+module.exports = Thought;
